6284948a941a7f58b06b46989fa2aff1408ce159
Ashik-AD <looge321@gmail.com>
2023-07-12 14:44:31 +0545

    chore: cleanup & optimize parallax

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-var _a = document.querySelectorAll('.bg__img-sky, .bg__img-mountain, .bg__img-human'), imgSky = _a[0], imgMountain = _a[1], imgHuman = _a[2];
-var cta = document.querySelector('.cta');
+var _a = document.querySelectorAll(".bg__img-sky, .bg__img-mountain, .bg__img-human"), imgSky = _a[0], imgMountain = _a[1], imgHuman = _a[2];
+var cta = document.querySelector(".cta");
 var screenHeight = window.screen.availHeight;
-var scrollBtn = document.querySelector('.btn__scroll');
-var loading = document.querySelector('.loading');
+var scrollBtn = document.querySelector(".btn__scroll");
+var loading = document.querySelector(".loading");
 setTimeout(function () {
-    loading.className += ' loading-hidden';
+    loading.className += " loading-hidden";
 }, 2000);
-scrollBtn === null || scrollBtn === void 0 ? void 0 : scrollBtn.addEventListener('click', function () {
-    var offsetTop = document.querySelector('.main__content').offsetTop;
+scrollBtn === null || scrollBtn === void 0 ? void 0 : scrollBtn.addEventListener("click", function () {
+    var offsetTop = document.querySelector(".main__content").offsetTop;
     function scrollToMainContent() {
         var scrollTop = window.scrollY;
         window.scrollTo({ top: scrollTop + 26 });
@@ -20,15 +20,17 @@ scrollBtn === null || scrollBtn === void 0 ? void 0 : scrollBtn.addEventListener
 function parallax() {
     var scrollY = window.scrollY;
     if (scrollY > screenHeight) {
-        cta.style.opacity = '0';
+        cta.style.opacity = "0";
         return;
     }
     var opacity = (((screenHeight / 2) - scrollY) / 4) / 100;
     var shiftBy = (scrollY * 0.1) / 10;
     cta.style.opacity = "".concat(opacity);
     imgSky.style.transform = "translate3d(0px, ".concat(-(shiftBy * 7), "%, 0px)");
-    imgMountain.style.transform = "translate3d(0px, ".concat(-(shiftBy * 2.5), "%, 0px)");
+    imgMountain.style.transform = "translate3d(0px, ".concat(-(shiftBy * 4.5), "%, 0px)");
     imgHuman.style.transform = "translate3d(0px, ".concat(-(shiftBy), "%, 0px)");
+    requestAnimationFrame(parallax);
 }
-parallax();
-window.addEventListener('scroll', parallax);
+window.addEventListener("scroll", function () {
+    requestAnimationFrame(parallax);
+});
